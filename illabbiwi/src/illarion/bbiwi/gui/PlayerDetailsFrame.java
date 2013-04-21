66e61fc1752903dd07cea7dba5e2b5df26c2b218
Martin Karing <nitram@illarion.org>
2013-04-21 10:37:36 +0200

    [BBIBI] Added first test for player details

--- illabbiwi/src/illarion/bbiwi/gui/PlayerDetailsFrame.java
+++ illabbiwi/src/illarion/bbiwi/gui/PlayerDetailsFrame.java
@@ -18,10 +18,18 @@
  */
 package illarion.bbiwi.gui;
 
+import illarion.bbiwi.events.PlayerAttributeChangedEvent;
 import illarion.bbiwi.world.Player;
+import illarion.common.data.CharacterAttribute;
+import org.bushe.swing.event.EventServiceLocator;
+import org.bushe.swing.event.annotation.AnnotationProcessor;
+import org.bushe.swing.event.annotation.EventSubscriber;
 import org.jdesktop.swingx.JXFrame;
+import org.jdesktop.swingx.JXLabel;
+import org.jdesktop.swingx.JXPanel;
 
 import javax.annotation.Nonnull;
+import java.awt.*;
 
 /**
  * This frame contains details regarding a single player.
@@ -32,7 +40,17 @@ public class PlayerDetailsFrame extends JXFrame {
     /**
      * The player that is shown in this frame.
      */
+    @Nonnull
     private final Player player;
+
+    @Nonnull
+    private final JXLabel hitPointsLabel;
+
+    @Nonnull
+    private final JXLabel manaPointsLabel;
+
+    @Nonnull
+    private final JXLabel foodPointsLabel;
 
     /**
      * Create a new player details frame and set the player that is supposed to be displayed in this frame.
@@ -42,5 +60,48 @@ public class PlayerDetailsFrame extends JXFrame {
     public PlayerDetailsFrame(@Nonnull final Player player) {
         super(player.getName());
         this.player = player;
+
+        final Container content = getContentPane();
+        content.setLayout(new FlowLayout(FlowLayout.LEADING));
+
+        final JXPanel hitPointsPanel = new JXPanel(new BorderLayout());
+        hitPointsPanel.add(new JXLabel("HitPoints:"), BorderLayout.WEST);
+        hitPointsLabel = new JXLabel(Integer.toString(player.getAttribute(CharacterAttribute.HitPoints)));
+        hitPointsPanel.add(hitPointsLabel, BorderLayout.CENTER);
+        content.add(hitPointsPanel);
+
+        final JXPanel manaPointsPanel = new JXPanel(new BorderLayout());
+        manaPointsPanel.add(new JXLabel("Mana:"), BorderLayout.WEST);
+        manaPointsLabel = new JXLabel(Integer.toString(player.getAttribute(CharacterAttribute.ManaPoints)));
+        manaPointsPanel.add(manaPointsLabel, BorderLayout.CENTER);
+        content.add(manaPointsPanel);
+
+        final JXPanel foodPointsPanel = new JXPanel(new BorderLayout());
+        foodPointsPanel.add(new JXLabel("Foodpoints:"), BorderLayout.WEST);
+        foodPointsLabel = new JXLabel(Integer.toString(player.getAttribute(CharacterAttribute.FoodPoints)));
+        foodPointsPanel.add(foodPointsLabel, BorderLayout.CENTER);
+        content.add(foodPointsPanel);
+
+        pack();
+
+        AnnotationProcessor.process(this);
+    }
+
+    @EventSubscriber(eventClass = PlayerAttributeChangedEvent.class,
+            eventServiceName = EventServiceLocator.SERVICE_NAME_SWING_EVENT_SERVICE)
+    public void onPlayerAttributeChangedEvent(@Nonnull final PlayerAttributeChangedEvent event) {
+        if (event.getPlayer().equals(player)) {
+            switch (event.getAttribute()) {
+                case HitPoints:
+                    hitPointsLabel.setText(Integer.toString(event.getNewValue()));
+                    break;
+                case ManaPoints:
+                    manaPointsLabel.setText(Integer.toString(event.getNewValue()));
+                    break;
+                case FoodPoints:
+                    foodPointsLabel.setText(Integer.toString(event.getNewValue()));
+                    break;
+            }
+        }
     }
 }
